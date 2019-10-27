d929e45addff8f5b66d713ded2ae0f99403d0a17
ElieM <emmehandes@gmail.com>
2019-10-27 22:27:19 +0000

    Added: - error checking for field prompt in configuration - default configuration

--- src/settings/config.rs
+++ src/settings/config.rs
@@ -1,6 +1,7 @@
 use serde_json::Value;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
 
 pub struct Config {
     pub prompt: String,
@@ -8,10 +9,25 @@ pub struct Config {
 
 // Read and deserialize configuration
 pub fn read_config(config_path: &str) -> Config {
-    let file = File::open(config_path).unwrap();
-    let reader = BufReader::new(file);
-    let config_file: Value = serde_json::from_reader(reader).unwrap();
-    let config_shell = Config {
-            prompt: config_file["prompt"].as_str().unwrap().to_string() };
-    config_shell
+    let config  =
+        if Path::new(config_path).exists() {
+            let file = File::open(config_path).unwrap();
+            let reader = BufReader::new(file);
+            let config_file  = serde_json::from_reader(reader);
+            if config_file.is_ok() {
+                let config: Value = config_file.unwrap();
+                Config { prompt: config["prompt"].as_str()
+                                         .unwrap()
+                                         .to_string() }
+            }
+            else {
+                eprintln!("Configuration Error: {}: {:?}",
+                          config_path,
+                          config_file.err().unwrap().to_string());
+                Config { prompt: "$ ".to_string() }
+            }
+        } else {
+            Config { prompt: "$ ".to_string() }
+        };
+    config
 }
